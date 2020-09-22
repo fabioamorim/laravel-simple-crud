432bae8c318ba7054b4b2050e769e0ceb2dd340c
Fábio Amorim <batista.amorim@hotmail.com>
2020-09-22 20:55:31 -0300

    Add User model, it is a Elloquent model

---
+++ simple-crud/app/Users.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model 
+{
+    public $timestamps = false;
+    protected $table = 'user';
+    protected $fillable = [
+        'name',
+        'cpf',
+        'email'
+    ];
+}
